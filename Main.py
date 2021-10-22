c0221257c65dcb03d0841e2ea98f4cfed413fa32
swetha7799 <56219464+swetha7799@users.noreply.github.com>
2021-10-22 18:18:29 +0530

    first commit
    first commit

---
+++ Main.py
@@ -0,0 +1,21 @@
+from dataProc import *
+from Threads import *
+
+def main():
+    EncryptInput()
+    
+if __name__=="__main__":
+    main()
+
+
+def EncryptInput():
+	Segment()
+	getInfo()
+	Crypt()
+
+
+def DecryptMessage():
+    DeCrypt()
+    trim()
+    Merge()
+        
